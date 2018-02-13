467d0be0b4d04a825926bc5b25c5df54a513a15f
Kayla Webb <kaylarwebb@gmail.com>
2018-02-13 10:32:09 -0500

    Create another v-for with links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,13 +2,20 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul id="vue-links">
-      <li v-for="(link, index) in links" v-bind:link='link' v-bind:index='index'
-        v-bind:title='link.title'
-        v-bind:key=index>
-       <a :href='link.url' target="_blank"> {{ link.title }} </a>
-      </li>
-    </ul>
+      <ul id="vue-core-links">
+        <li v-for="(link, index) in corelinks" v-bind:index='index'
+          v-bind:title='link.title'
+          v-bind:key=index>
+        <a :href='link.url' target="_blank"> {{ link.title }} </a>
+        </li>
+      </ul>
+    <h2>Ecosystem</h2>
+      <ul id="vue-eco-links">
+        <li v-for="(link, index) in ecoLinks" v-bind:index='index' v-bind:title="link.title"
+          v-bind:key=index>
+          <a :href='link.url' target="_blank"> {{ link. title }} </a>
+        </li>
+      </ul>
   </div>
 </template>
 
@@ -17,10 +24,18 @@ export default {
   name: 'HelloWorld',
   data: () => ({
     msg: 'Kayla`s app',
-    links: [
+    corelinks: [
       { url: 'https://vuejs.org', title: 'Core Docs' },
       { url: 'https://forum.vuejs.org', title: 'Forum' },
       { url: 'https://chat.vuejs.org', title: 'Community Docs' },
+      { url: 'https://twitter.com/vuejs', title: 'Twitter' },
+      { url: 'http://vuejs-templates.github.io/webpack/', title: 'Docs for This Template' },
+    ],
+    ecoLinks: [
+      { url: 'http://router.vuejs.org/', title: 'vue-router' },
+      { url: 'http://vuex.vuejs.org/', title: 'vuex' },
+      { url: 'http://vue-loader.vuejs.org/', title: 'vue-loader' },
+      { url: 'https://github.com/vuejs/awesome-vue', title: 'awesome-vue' },
     ],
   }),
 };
